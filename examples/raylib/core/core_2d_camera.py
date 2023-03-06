5bfc4ebc4ce2effbcc0943c4d3a5a7455b1e3b61
דור שפירא <Dor.Shapira@outlook.com>
2023-03-06 20:23:33 +0200

    refactor the core_2d_camera by the #5 and #6 issues fixes

--- examples/raylib/core/core_2d_camera.py
+++ examples/raylib/core/core_2d_camera.py
@@ -4,7 +4,7 @@ raylib [core] example - 2d camera
 
 """
 
-from raylib import *
+import raylib
 
 # Definitions
 # ------------------------------------------------------------------------------------
@@ -22,49 +22,45 @@ def main():
     SCREEN_WIDTH = 800
     SCREEN_HEIGHT = 450
 
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib [core] example - 2d camera")
+    raylib.InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib [core] example - 2d camera")
 
-    player = Rectangle(400, 280, 40, 40)
+    player = raylib.Rectangle(400, 280, 40, 40)
     buildings = []
     build_colors = []
 
     spacing = 0
 
     for i in range(MAX_BUILDINGS):
-        buildings.append(Rectangle())
-        buildings[i].width = GetRandomValue(50, 200)
-        buildings[i].height = GetRandomValue(100, 800)
+        buildings.append(raylib.Rectangle())
+        buildings[i].width = raylib.GetRandomValue(50, 200)
+        buildings[i].height = raylib.GetRandomValue(100, 800)
         buildings[i].y = SCREEN_HEIGHT - 130.0 - buildings[i].height
         buildings[i].x = -6000.0 + spacing
 
         spacing += buildings[i].width
-        build_colors.append(Color())
-        build_colors[i] = Color(GetRandomValue(200, 240), GetRandomValue(200, 240), GetRandomValue(200, 250), 255)
+        build_colors.append(raylib.Color())
+        build_colors[i] = raylib.Color(raylib.GetRandomValue(200, 240), raylib.GetRandomValue(200, 240), raylib.GetRandomValue(200, 250), 255)
 
-    camera = Camera2D()
-    camera.target = Vector2(player.x + 20.0, player.y + 20.0)
-    camera.offset = Vector2(SCREEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0)
-    camera.rotation = 0.0
-    camera.zoom = 1.0
+    camera = raylib.Camera2D(raylib.Vector2(player.x + 20.0, player.y + 20.0), raylib.Vector2(SCREEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0), 0.0, 1.0)
 
-    SetTargetFPS(60)  # Set our game to run at 60 frames-per-second
+    raylib.SetTargetFPS(60)  # Set our game to run at 60 frames-per-second
     # ------------------------------------------------------------------------------------
 
     # Main game loop
-    while not WindowShouldClose():  # Detect window close button or ESC key
+    while not raylib.WindowShouldClose():  # Detect window close button or ESC key
         # Update
         # ----------------------------------------------------------------------------------
         # Player movement
-        if IsKeyDown(KEY_RIGHT): player.x += 2
-        if IsKeyDown(KEY_LEFT): player.x -= 2
+        if raylib.IsKeyDown(raylib.KEY_RIGHT): player.x += 2
+        if raylib.IsKeyDown(raylib.KEY_LEFT): player.x -= 2
 
         # Camera target follows player
-        camera.target = Vector2(player.x + 20, player.y + 20)
+        camera.target = raylib.Vector2(player.x + 20, player.y + 20)
 
         # Camera rotation controls
-        if IsKeyDown(KEY_A):
+        if raylib.IsKeyDown(raylib.KEY_A):
             camera.rotation -= 1
-        elif IsKeyDown(KEY_S):
+        elif raylib.IsKeyDown(raylib.KEY_S):
             camera.rotation += 1
 
         # Limit camera rotation to 80 degrees (-40 to 40)
@@ -74,7 +70,7 @@ def main():
             camera.rotation = -40
 
         # Camera zoom controls
-        camera.zoom += GetMouseWheelMove() * 0.05
+        camera.zoom += raylib.GetMouseWheelMove() * 0.05
 
         if camera.zoom > 3.0:
             camera.zoom = 3.0
@@ -82,52 +78,52 @@ def main():
             camera.zoom = 0.1
 
         # Camera reset (zoom and rotation)
-        if IsKeyPressed(KEY_R):
+        if raylib.IsKeyPressed(raylib.KEY_R):
             camera.zoom = 1.0
             camera.rotation = 0.0
         # ----------------------------------------------------------------------------------
 
         # Draw
         # ----------------------------------------------------------------------------------
-        BeginDrawing()
+        raylib.BeginDrawing()
 
-        ClearBackground(RAYWHITE)
+        raylib.ClearBackground(raylib.RAYWHITE)
 
-        BeginMode2D(camera)
+        raylib.BeginMode2D(camera)
 
-        DrawRectangle(-6000, 320, 13000, 8000, DARKGRAY)
+        raylib.DrawRectangle(-6000, 320, 13000, 8000, raylib.DARKGRAY)
 
-        for i in range(MAX_BUILDINGS): DrawRectangleRec(buildings[i], build_colors[i])
+        for i in range(MAX_BUILDINGS): raylib.DrawRectangleRec(buildings[i], build_colors[i])
 
-        DrawRectangleRec(player, RED)
+        raylib.DrawRectangleRec(player, raylib.RED)
 
-        DrawLine(int(camera.target.x), -SCREEN_HEIGHT * 10, int(camera.target.x), SCREEN_HEIGHT * 10, GREEN)
-        DrawLine(-SCREEN_WIDTH * 10, int(camera.target.y), SCREEN_WIDTH * 10, int(camera.target.y), GREEN)
+        raylib.DrawLine(int(camera.target.x), -SCREEN_HEIGHT * 10, int(camera.target.x), SCREEN_HEIGHT * 10, raylib.GREEN)
+        raylib.DrawLine(-SCREEN_WIDTH * 10, int(camera.target.y), SCREEN_WIDTH * 10, int(camera.target.y), raylib.GREEN)
 
-        EndMode2D()
+        raylib.EndMode2D()
 
-        DrawText("SCREEN AREA", 640, 10, 20, RED)
+        raylib.DrawText("SCREEN AREA", 640, 10, 20, raylib.RED)
 
-        DrawRectangle(0, 0, SCREEN_WIDTH, 5, RED)
-        DrawRectangle(0, 5, 5, SCREEN_HEIGHT - 10, RED)
-        DrawRectangle(SCREEN_WIDTH - 5, 5, 5, SCREEN_HEIGHT - 10, RED)
-        DrawRectangle(0, SCREEN_HEIGHT - 5, SCREEN_WIDTH, 5, RED)
+        raylib.DrawRectangle(0, 0, SCREEN_WIDTH, 5, raylib.RED)
+        raylib.DrawRectangle(0, 5, 5, SCREEN_HEIGHT - 10, raylib.RED)
+        raylib.DrawRectangle(SCREEN_WIDTH - 5, 5, 5, SCREEN_HEIGHT - 10, raylib.RED)
+        raylib.DrawRectangle(0, SCREEN_HEIGHT - 5, SCREEN_WIDTH, 5, raylib.RED)
 
-        DrawRectangle(10, 10, 250, 113, Fade(SKYBLUE, 0.5))
-        DrawRectangleLines(10, 10, 250, 113, BLUE)
+        raylib.DrawRectangle(10, 10, 250, 113, raylib.Fade(raylib.SKYBLUE, 0.5))
+        raylib.DrawRectangleLines(10, 10, 250, 113, raylib.BLUE)
 
-        DrawText("Free 2d camera controls:", 20, 20, 10, BLACK)
-        DrawText("- Right/Left to move Offset", 40, 40, 10, DARKGRAY)
-        DrawText("- Mouse Wheel to Zoom in-out", 40, 60, 10, DARKGRAY)
-        DrawText("- A / S to Rotate", 40, 80, 10, DARKGRAY)
-        DrawText("- R to reset Zoom and Rotation", 40, 100, 10, DARKGRAY)
+        raylib.DrawText("Free 2d camera controls:", 20, 20, 10, raylib.BLACK)
+        raylib.DrawText("- Right/Left to move Offset", 40, 40, 10, raylib.DARKGRAY)
+        raylib.DrawText("- Mouse Wheel to Zoom in-out", 40, 60, 10, raylib.DARKGRAY)
+        raylib.DrawText("- A / S to Rotate", 40, 80, 10, raylib.DARKGRAY)
+        raylib.DrawText("- R to reset Zoom and Rotation", 40, 100, 10, raylib.DARKGRAY)
 
-        EndDrawing()
+        raylib.EndDrawing()
         # ----------------------------------------------------------------------------------
 
     # De-Initialization
     # ----------------------------------------------------------------------------------
-    CloseWindow()  # Close window and OpenGL context
+    raylib.CloseWindow()  # Close window and OpenGL context
     # ----------------------------------------------------------------------------------
 
 
